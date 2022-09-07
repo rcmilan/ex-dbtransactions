df5ff6a74ebb0ed5dac48eed91b11f41ab3cebc1
Ricardo Milan <ricardo@fluencypass.com>
2022-09-07 11:44:42 -0300

    migration

--- ex-dbtransactions/Migrations/20220907144419_department.Designer.cs
+++ ex-dbtransactions/Migrations/20220907144419_department.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ex_dbtransactions.Database;
 
@@ -10,9 +11,10 @@ using ex_dbtransactions.Database;
 namespace ex_dbtransactions.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220907144419_department")]
+    partial class department
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
